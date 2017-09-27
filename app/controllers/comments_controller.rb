0be5a6779c17d28377c842d07f5f735660aaffab
Sam Gardiner <nekura.gesshoku@gmail.com>
2017-09-27 00:37:39 +0000

    created comment controller nested in places

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+	before_action :authenticate_user!
+
+
+	def create
+		@place = Place.find(params[:place_id])
+		@place.comments.create(comments_params.merge(user: current_user))
+		redirect_to place_path(@place)
+	end
+
+
+	private
+
+
+	def comment_params
+		params.require(:comment).permit(:message, :rating)
+	end
+end
